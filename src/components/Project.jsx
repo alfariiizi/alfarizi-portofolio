1ff7ef9fa82d7ead554567a25d0ffc663b93f331
alfariiizi <alfariiiziiiii@gmail.com>
2023-02-19 01:08:30 +0700

    Add project card

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Devider from "./Devider";
+import ProjectCard from "./ProjectCard";
 
 const Project = () => {
   return (
     <div className="">
       <Devider name={"My Work"} />
-      <div className="p-5 dark:bg-gradient-to-bl dark:from-slate-800 dark:to-slate-900 dark:text-slate-200">
-        All Project
+      <div className="p-5 dark:bg-gradient-to-bl dark:from-slate-800 dark:to-slate-900">
+        <div className="mx-auto grid w-3/5 justify-center gap-5 lg:grid-cols-2 xl:grid-cols-3">
+          <ProjectCard href="/" label="Some text" />
+          <ProjectCard href="/" label="Some text" />
+          <ProjectCard href="/" label="Some text" />
+          <ProjectCard href="/" label="Some text" />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ProjectCard = ({ imgSrc, label, href, className }) => {
+  return (
+    <div>
+      <div className="flex h-[300px] w-[256px] flex-col items-center gap-2 overflow-hidden rounded-lg border-2 border-[#0b2149] bg-gray-200 dark:border-slate-200">
+        <a
+          href={href}
+          className="h-[256px] w-[256px] overflow-hidden bg-blue-500 hover:bg-blue-600">
+          <img src={imgSrc} alt="" className={className} />
+        </a>
+        <p>{label}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
