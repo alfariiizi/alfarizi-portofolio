4123f39f2d2a9044f83721280d1ae63107787356
alfariiizi <alfariiiziiiii@gmail.com>
2023-02-19 12:04:17 +0700

    Adding y-margin on 'project' section

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -9,7 +9,7 @@ const Project = () => {
     <div className="">
       <Devider name={"My Work"} />
       <div className="p-5 dark:bg-gradient-to-bl dark:from-slate-800 dark:to-slate-900">
-        <div className="mx-auto grid w-3/5 justify-center gap-5 lg:grid-cols-2 xl:grid-cols-3">
+        <div className="mx-auto my-8 grid w-3/5 justify-center gap-5 lg:grid-cols-2 xl:grid-cols-3">
           {projects.map((project, index) => {
             return (
               <ProjectCard
