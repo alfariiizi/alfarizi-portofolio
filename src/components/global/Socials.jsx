7cd5a1b9eeb0120f489f3dd71601d5b2286457e3
alfariiizi <alfariiiziiiii@gmail.com>
2023-03-05 22:53:35 +0700

    Correcting 'key' props on Socials component

--- src/components/global/Socials.jsx
+++ src/components/global/Socials.jsx
@@ -7,13 +7,12 @@ const Socials = () => {
     <div className="relative z-[100] hidden font-inter text-slate-200 md:block">
       <ul className="fixed left-0 flex h-screen w-[140px] flex-col justify-center">
         {socialLinks.map((socialLink, index) => (
-          <li>
+          <li key={index}>
             <a
               href={socialLink.link}
               target="_blank"
               rel="noreferrer"
-              className={`${socialLink.customClass} relative -left-[94px] z-40 flex items-center justify-between bg-blue-800 p-2 transition-all duration-300 hover:left-0`}
-              key={index}>
+              className={`${socialLink.customClass} relative -left-[94px] z-40 flex items-center justify-between bg-blue-800 p-2 transition-all duration-300 hover:left-0`}>
               {socialLink.name}
               <socialLink.logo size={30} />
             </a>
